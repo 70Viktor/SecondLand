ff4464c6b9d614b05779cf99b64172efcebb5234
Viktor <94440460+70Viktor@users.noreply.github.com>
2022-02-08 20:57:04 +0700

    add scroll-animations on native js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,37 @@
+// parallax
 function parallax(event) {
 	this
 	.querySelector('.mainpage__background')
 	.style
 	.transform = `translateX(${event.clientX/50}px)`
-	console.log(event.clientX)
 }
 
-
-
 document.addEventListener('mousemove', parallax)
-
+// scroll botton
 let anchor = document.getElementById('scroll')
+
 anchor.addEventListener("click",function (event) {
 	document.getElementById('section1').scrollIntoView({
 		behavior: "smooth",
 		block: "start"
 	})
 })
+
+// animation
+const animItems = document.querySelectorAll('._anim-items');
+let animOnScroll = () => {
+	animItems.forEach(el => {
+		let height = el.getBoundingClientRect().height
+	if (el.getBoundingClientRect().top <= window.innerHeight*3/4
+	&&(el.getBoundingClientRect().bottom > window.innerHeight/4)) {
+		el.classList.add('_active')
+	} else {
+		el.classList.remove('_active')
+	}
+	})
+}
+
+	animOnScroll()
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll)
+	}
